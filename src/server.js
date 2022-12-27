9501fc51955cc20f3b9fa8b447ef698a430f31b2
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-12-27 13:19:08 -0300

    feat: delete book by id

--- src/server.js
+++ src/server.js
@@ -26,9 +26,9 @@ server.get('/books/:id', (req, res) => {
 	const id = req.params.id
 	const idx = findBook(id)
 	if (books[idx]) {
-		res.status(200).json({...books[idx]})
+		res.status(200).json({ ...books[idx] })
 	} else {
-		res.status(404).json({message: `Book #${id} not found`})
+		res.status(404).json({ message: `Book #${id} not found` })
 	}
 })
 
@@ -39,13 +39,13 @@ server.post('/books', (req, res) => {
 	))
 	if (!hasBook) {
 		book = {
-		   id: books.length + 1,
-		   ...book
-	   }
-	   books.push(book)
-	   res.status(201).json({
-		   message: 'Book created successfully', book: {...book}
-	   })
+			id: books.length + 1,
+			...book
+		}
+		books.push(book)
+		res.status(201).json({
+			message: 'Book created successfully', book: { ...book }
+		})
 	} else {
 		res.status(406).json({
 			message: `Book '${req.body.title}' already exists`
@@ -59,10 +59,21 @@ server.put('/books/:id', (req, res) => {
 	if (books[idx]) {
 		books[idx].title = req.body.title
 		res.status(200).json({
-			message: 'Book updated successfully', book: {...books[idx]}
+			message: 'Book updated successfully', book: { ...books[idx] }
 		})
 	} else {
-		res.status(404).json({message: `Book #${id} not found`})
+		res.status(404).json({ message: `Book #${id} not found` })
+	}
+})
+
+server.delete('/books/:id', (req, res) => {
+	const id = req.params.id
+	const idx = findBook(id)
+	if (books[idx]) {
+		books.splice(idx, 1)
+		res.status(200).json({ message: `Book #${id} deleted successfully` })
+	} else {
+		res.status(404).json({ message: `Book #${id} not found` })
 	}
 })
 
