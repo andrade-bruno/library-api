e1bb2482243337f952713180cb125df3b0c90fb2
Bruno Andrade <brunoandrade.contato@icloud.com>
2022-12-27 12:29:58 -0300

    feat: get book by id, update book by id

--- src/server.js
+++ src/server.js
@@ -22,6 +22,16 @@ server.get('/books', (req, res) => {
 	res.status(200).json(books)
 })
 
+server.get('/books/:id', (req, res) => {
+	const id = req.params.id
+	const idx = findBook(id)
+	if (books[idx]) {
+		res.status(200).json({...books[idx]})
+	} else {
+		res.status(404).json({message: `Book #${id} not found`})
+	}
+})
+
 server.post('/books', (req, res) => {
 	let book = req.body
 	const hasBook = books.some(item => (
@@ -42,5 +52,22 @@ server.post('/books', (req, res) => {
 		})
 	}
 })
+
+server.put('/books/:id', (req, res) => {
+	const id = req.params.id
+	const idx = findBook(id)
+	if (books[idx]) {
+		books[idx].title = req.body.title
+		res.status(200).json({
+			message: 'Book updated successfully', book: {...books[idx]}
+		})
+	} else {
+		res.status(404).json({message: `Book #${id} not found`})
+	}
+})
+
+function findBook(id) {
+	return books.findIndex(book => book.id == id)
+}
 
 export default server
